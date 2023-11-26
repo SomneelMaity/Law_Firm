24e55e759388113c52c6fce378c501f514a90228
SomneelMaity <somneel.maity@gmail.com>
2023-11-26 23:16:44 +0530

    Complete

--- lawfirm/src/App.js
+++ lawfirm/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 import Choose from './components/Choose/Choose';
+import GridSection from './components/GridSection/GridSection';
 import HeroSection from './components/HeroSection/HeroSection';
 import MainBodyTopText from './components/MainBodyTopText/MainBodyTopText';
+import Members from './components/Members/Members';
 import Navbar from './components/Navbar/Navbar';
+import PeopleBox from './components/PeopleBox/PeopleBox';
+import Faq from './components/FAQ/Faq';
+import Subscribe from './components/Subscribe/Subscribe';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -13,6 +19,12 @@ function App() {
       </div>
       <MainBodyTopText />
       <Choose />
+      <GridSection />
+      <PeopleBox />
+      <Members />
+      <Faq />
+      <Subscribe />
+      <Footer />
     </div>
   );
 }
